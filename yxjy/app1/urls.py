366e5326568bae92b9a8c16a3ff6cbe35dab643a
wang <1359785501@qq.com>
2019-01-21 16:13:11 +0800

    项目1

---
+++ yxjy/app1/urls.py
@@ -0,0 +1,5 @@
+from app1.views import *
+from django.urls import path
+urlpatterns = [
+    path('to/',may)
+]
---
+++ yxjy/app1/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def may(request):
+    return render(request,'index.html')
