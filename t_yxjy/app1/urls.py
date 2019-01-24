6fe9817149a9632eeea032df1bbf8afd3e67a326
tjl <897145810@qq.com>
2019-01-24 15:32:09 +0800

    1.24

--- t_yxjy/app1/urls.py
+++ t_yxjy/app1/urls.py
@@ -2,8 +2,18 @@ from app1.views import *
 from django.urls import path
 
 app_name = 'yxjy'
+
+
+
+
 urlpatterns = [
     path('to/',may),
     path('deal/',go_account_deal,name= 'deal'),
+    path('go_details',go_details,name= 'g_detail'),
     path('purchase/',go_account_purchase,name= 'pur'),
+    path('a_order/',a_order,name= 'a_order'),
+    path('p_order/',p_order,name= 'p_order'),
+    path('p_code/',pay_code,name= 'p_code'),
+    path('go_s/',go_success,name= 'g_su'),
+
 ]
